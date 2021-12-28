d1f6fb00f9ac0a7937bc5b9ba7a5f55c09998798
dagnaldo <naldo.penha@gmail.com>
2021-12-28 20:18:57 -0300

    pep8 formatting.

--- manage.py
+++ manage.py
@@ -6,7 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'service_load_data_elasticsearch.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                          'service_load_data_elasticsearch.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
