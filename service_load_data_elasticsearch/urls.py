de39fd3d5aa7d0151830cc3865cba531c3db5390
dagnaldo <naldo.penha@gmail.com>
2021-12-28 20:20:07 -0300

    renaming path to core.urls

--- service_load_data_elasticsearch/urls.py
+++ service_load_data_elasticsearch/urls.py
@@ -16,5 +16,5 @@ Including another URLconf
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('core.urls')),
+    path('detection/', include(('core.urls', 'core'), namespace='core')),
 ]
