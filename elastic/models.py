5fa7fc3ca09750f370a2cb883cd699a548e68475
dagnaldo <naldo.penha@gmail.com>
2022-02-09 15:22:15 -0300

    Adding help text to model data.

--- elastic/models.py
+++ elastic/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.serializers import json
+from django.utils.translation import ugettext_lazy as _
 
 
 class Structure(models.Model):
@@ -8,14 +9,33 @@ class Structure(models.Model):
     Stores in a sqlite database.
     """
 
-    identifier = models.CharField(max_length=255)
-    index = models.CharField(max_length=255)
-    url = models.CharField(max_length=255)
-    bulk_size_request = models.IntegerField(default=1000)
-    structure = models.JSONField(encoder=json.DjangoJSONEncoder)
+    identifier = models.CharField(
+        _('Identifier'),
+        max_length=255
+    )
+
+    index = models.CharField(
+        _('Index'),
+        max_length=255
+    )
+
+    url = models.CharField(
+        _('Structure url'),
+        max_length=255
+    )
+
+    bulk_size_request = models.IntegerField(
+        _('Bulk size per request'),
+        default=1000
+    )
+
+    structure = models.JSONField(
+        _('Json Structure'),
+        encoder=json.DjangoJSONEncoder
+    )
 
     class Meta:
-        ordering = ['url', 'identifier']
+        ordering = ('url', 'identifier', )
 
     def __str__(self):
         return f'{self.identifier}'
